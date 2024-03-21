ce9f6b6145b83ccece148e5ccd086dc944f9b540
P小二 <xxg1413@gmail.com>
2024-03-22 00:30:07 +0800

    add plan

--- 02.ChatWithGemma7b /gemmachat.py
+++ 02.ChatWithGemma7b /gemmachat.py
@@ -1,11 +1,10 @@
-# pip install accelerate
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b-it")
-model = AutoModelForCausalLM.from_pretrained("google/gemma-7b-it", device_map="auto")
+model = AutoModelForCausalLM.from_pretrained("google/gemma-7b-it")
 
 input_text = "Write me a poem about Machine Learning."
-input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+input_ids = tokenizer(input_text, return_tensors="pt")
 
 outputs = model.generate(**input_ids)
 print(tokenizer.decode(outputs[0]))
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # FlyPythonLLM
-Code of FlyPython Newsletter
+
+Code of [FlyPython Newsletter](https://blog.flypython.com/)
+
+
+## free content
+1. GPTChat
+2. ChatWithGemma7b
+3. GPTs Action (1)
+4. GPTs Action (2)
+5. LangChain(1)
+6. LangChain(2)
+
+
+
+
+## paid content
+1. MoneyPrinter
+2. mindgraph
+3. metavoice
